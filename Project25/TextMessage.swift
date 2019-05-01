0c409fe79ac6058f0747e5c0a9331fa04b89c670
Kirsty Darbyshire <kirsty@loquax.com>
2019-05-01 15:04:23 +0100

    challenges finished

---
+++ Project25/TextMessage.swift
@@ -0,0 +1,14 @@
+//
+//  TextMessage.swift
+//  Project25
+//
+//  Created by kirsty darbyshire on 01/05/2019.
+//  Copyright © 2019 nocto. All rights reserved.
+//
+
+import Foundation
+
+struct TextMessage: Codable {
+    var sender: String
+    var message: String
+}
--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     
     var images = [UIImage]()
+    var textMessages = [TextMessage]() // I was planning to make a view to show all messages but haven't got to it (yet?)
     
     var peerID = MCPeerID(displayName: UIDevice.current.name)
     var mcSession: MCSession?
@@ -22,8 +23,13 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         // Do any additional setup after loading the view.
         
         title = "Selfie Share"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+        let cameraButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        let messageButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(writeMessage))
+        navigationItem.rightBarButtonItems = [cameraButton, messageButton]
+        
+        let connectionButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+        let peersButton = UIBarButtonItem(title: "Who?", style: .plain, target: self, action: #selector(showPeers))
+        navigationItem.leftBarButtonItems = [connectionButton, peersButton]
         
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         mcSession?.delegate = self
@@ -48,6 +54,18 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
         ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    @objc func showPeers() {
+        guard let mcSession = mcSession else { return }
+        let peers = mcSession.connectedPeers
+        var peernames = "You! on \(peerID.displayName)"
+        for mcPeerID in peers {
+            peernames += "\n" + mcPeerID.displayName
+        }
+        let ac = UIAlertController(title: "Who is here?", message: peernames, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
 
@@ -78,6 +96,39 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             }
         }
     }
+    
+    @objc func writeMessage() {
+        let ac = UIAlertController(title: "Write Message", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Send", style: .default) {
+            [weak self] _ in
+            guard let message = ac.textFields?[0].text else { return }
+            self?.sendMessage(message)
+            })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func sendMessage(_ message: String) {
+        let textMessage = TextMessage(sender: peerID.displayName, message: message)
+        textMessages.insert(textMessage, at: 0)
+        
+        guard let mcSession = mcSession else { return }
+        
+        if mcSession.connectedPeers.count > 0 {
+            do {
+                let encoder = JSONEncoder()
+                let encodedMessage = try encoder.encode(textMessage)
+                let messageData = Data(encodedMessage)
+                try mcSession.send(messageData, toPeers: mcSession.connectedPeers, with: .reliable)
+            } catch {
+                let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            }
+        }
+    }
+
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
@@ -123,7 +174,7 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             print("Not connected: \(peerID.displayName)")
             DispatchQueue.main.async {
                 [weak self] in
-                let ac = UIAlertController(title: "Disconnection", message: "\(String(describing: self?.peerID.displayName)) has disconnected", preferredStyle: .alert)
+                let ac = UIAlertController(title: "Disconnection", message: "\(String(describing: peerID.displayName)) has disconnected", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
                 self?.present(ac, animated: true)
             }
@@ -136,11 +187,35 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
         DispatchQueue.main.async {
             [weak self] in
+            // first see if what's been received is an image
             if let image = UIImage(data: data) {
                 self?.images.insert(image, at: 0)
                 self?.collectionView.reloadData()
+                return
             }
+            // now see if it was a text message
+            if let receivedMessage = self?.decodeTextMessage(data: data) {
+                let ac = UIAlertController(title: "Message from \(receivedMessage.sender)", message: receivedMessage.message, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                self?.present(ac, animated: true)
+                self?.textMessages.insert(receivedMessage, at: 0)
+                return
+            }
+
+            print ("something was received that I haven't managed to process \(data)")
         }
+    }
+    
+    func decodeTextMessage(data: Data) -> TextMessage? {
+        let decoder = JSONDecoder()
+        do {
+            let textMessage = try decoder.decode(TextMessage.self, from: data)
+            return textMessage
+        }
+        catch {
+            print("Error while trying to decode a text message")
+        }
+        return nil
     }
 }
 
